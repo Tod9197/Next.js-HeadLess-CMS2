3ee6ca93b6ac179f933cf851c3c811f3cd856333
Tod9197 <dancein18@yahoo.co.jp>
2025-02-19 19:56:43 +0900

    ページネーションの実装

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Category from "@/app/_components/Category";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 type Props = {
   params: {
@@ -14,6 +15,7 @@ export default async function Page({ params }: Props) {
   const category = await getCategoryDetail(params.id).catch(notFound);
 
   const { contents: news } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
     filters: `category[equals]${category.id}`,
   });
 
