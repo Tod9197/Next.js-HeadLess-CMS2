adf8652202ce0b15b9ebe945ecbb9c387534aa24
Tod9197 <dancein18@yahoo.co.jp>
2025-03-04 20:52:18 +0900

    metaデータ追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,24 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://next-js-head-less-cms-2.vercel.app/"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description: "Next.js + ヘッドレスCMSで始める!Webサイト",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description: "Next.js + ヘッドレスCMSで始める!Webサイト",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "https://next-js-head-less-cms-2.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
